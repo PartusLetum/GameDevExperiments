d9d325d56644bc61d0d25bb360aa47fab9ade6cf
Christopher New <Christopher.New@pollencap.com>
2018-06-21 17:41:20 +0100

    Start of 3d for the Win32 app.

--- Project1/Project1/WinMain.cpp
+++ Project1/Project1/WinMain.cpp
@@ -1,3 +1,7 @@
+//
+// http://real3d.pk/opengl_tuto_3.html
+//
+
 #include <stdio.h>
 #include <windows.h>
 
@@ -6,26 +10,58 @@ HBITMAP bufBMP;
 RECT rect;
 bool isRunning;
 
+struct Point3d { float x, y, z; };
+struct Face3d { unsigned int v1, v2, v3; };
+
+const int vertexCount = 3;		// Number of vertices in model
+Point3d vertices[3];			// Vertices of the model
+
+const int faceCount = 1;		// Number of faces in model
+Face3d faces[faceCount];		// Faces that make up the model (indicies into vertices)
+Point3d faceNormals[faceCount];	// Normals to the faces that make up the model
+
+void LoadModel()
+{
+	vertices[0].x = 10; vertices[0].y = 10; vertices[0].z = 0;
+	vertices[1].x = 90; vertices[1].y = 90; vertices[1].z = 0;
+	vertices[2].x = 10; vertices[2].y = 90; vertices[2].z = 0;
+
+	faces[0].v1 = 0; faces[0].v2 = 1; faces[0].v3 = 2;
+}
+
 void DrawLine(HDC hdc, COLORREF color, int x1, int y1, int x2, int y2)
 {
-	HPEN hPen = CreatePen(PS_SOLID, 3, color);
+	HPEN hPen = CreatePen(PS_SOLID, 1, color);
 	SelectObject(hdc, hPen);
 	MoveToEx(hdc, x1, y1, NULL);
 	LineTo(hdc, x2, y2);
 	DeleteObject(hPen);
 }
 
+void DrawModel(HDC hdc)
+{
+	for (int i = 0; i < faceCount; i++)
+	{
+		COLORREF color = RGB(255, 255, 255);
+		//color = RGB(100, 0, 0);
+		DrawLine(bufDC, color, (int)vertices[faces[i].v1].x, (int)vertices[faces[i].v1].y, (int)vertices[faces[i].v2].x, (int)vertices[faces[i].v2].y);
+		DrawLine(bufDC, color, (int)vertices[faces[i].v2].x, (int)vertices[faces[i].v2].y, (int)vertices[faces[i].v3].x, (int)vertices[faces[i].v3].y);
+		DrawLine(bufDC, color, (int)vertices[faces[i].v3].x, (int)vertices[faces[i].v3].y, (int)vertices[faces[i].v1].x, (int)vertices[faces[i].v1].y);
+	}
+}
+
 void Render(HDC hdc)
 {
-	int x1, y1, x2, y2;
-	x1 = rand() % 200;
-	y1 = rand() % 200;
-	x2 = rand() % 200;
-	y2 = rand() % 200;
-	COLORREF color = RGB(x1 % 255, x1 % 255, x1 % 255);
-	//color = RGB(100, 0, 0);
-	SetPixel(bufDC, x1, y1, color);  // random
-	//DrawLine(bufDC, color, x1, y1, x2, y2);
+	//int x1, y1, x2, y2;
+	//x1 = rand() % 200;
+	//y1 = rand() % 200;
+	//x2 = rand() % 200;
+	//y2 = rand() % 200;
+	//COLORREF color = RGB(x1 % 255, x1 % 255, x1 % 255);
+	////color = RGB(100, 0, 0);
+	//SetPixel(bufDC, x1, y1, color);  // random
+	////DrawLine(bufDC, color, x1, y1, x2, y2);
+	DrawModel(hdc);
 	BitBlt(hdc, 0, 0, rect.right, rect.bottom, bufDC, 0, 0, SRCCOPY);
 }
 
@@ -111,6 +147,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
 
 	isRunning = false;
 	HANDLE hRenderThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)RenderThread, hwnd, 0, NULL);
+	LoadModel();
 	isRunning = true;
 
 	// Event loop
